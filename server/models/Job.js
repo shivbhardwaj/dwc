339c44d549fa1cfd651ea23761eace59874274c5
LoLo8 <lnblackstock@gmail.com>
2016-11-03 12:13:57 -0700

    fixing minor info

--- server/models/Job.js
+++ server/models/Job.js
@@ -3,24 +3,24 @@ var mongoose = require("mongoose");
 var JobSchema = new mongoose.Schema({
 	// ----------------------employer information
 	// employerId:{type: String, required: true, minlength: 1},
-	employer: {type: String, required: true, minlength: 1},
-	employerName: {type: String, required: true, minlength: 1},
-	company: {type: String},
-	emailAddress: {type: String, required: true, minlength: 1},
+	firstName: {type: String, required: true, minlength: 1},
+	lastName: {type: String, required: true, minlength: 1},
+	companyName: {type: String},
 	physicalAddress: {type: String, required: true, minlength: 1},
 	city: {type: String, required: true, minlength: 1},
-	state: {type: String, required: true, minlength: 1},
+	zip: {type: String, required: true, minlength: 5},
 
 	// password: {type: String, required: true, minlength: 1}, //do we want this for recurring employers?
 	
 	phoneNumber: {type: String, required: true, minlength: 1},
+	emailAddress: {type: String, required: true, minlength: 1},
 
 	// ----------------------job information
 	startDate: {type: Date, required: true},
-	startTime:{type: Number, required: true, minlength: 3},
-	hours:{type: Number, required: true, minlength: 1},
+	startTime:{type: String, required: true, minlength: 3},
+	workHours:{type: Number, required: true, minlength: 1},
 	skillsNeeded: [{type: String, required: true}],
-	numberWorkers: {type: Number, required: true, minlength: 1},
+	numberOfWorkers: {type: Number, required: true, minlength: 1},
 	payRate: {type: Number, required: true, min: 15},
 	perks:[{type: String}],
 	englishNeeded: {type: string, required: true, minlength: 1}
