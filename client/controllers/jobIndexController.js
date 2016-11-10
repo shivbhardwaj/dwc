444bcb016a023caa4c163ded586cc4b02a7a4905
Joshua Peters <jpeters280@gmail.com>
2016-11-10 11:27:44 -0800

    staff index

--- client/controllers/jobIndexController.js
+++ client/controllers/jobIndexController.js
@@ -1,8 +1,15 @@
-DWCAppModule.controller('jobIndexController', ['$scope','jobsFactory', '$location', function($scope, jobsFactory, $location) {
+// DWCAppModule.controller('jobIndexController', ['$scope','jobsFactory', '$location', function($scope, jobsFactory, $location) {
 /*
   THIS INDEX METHOD ACCESSES THE JobS FACTORY AND RUNS THE JobS INDEX.
   WE MIGHT RE USE INDEX A FEW TIMES, SO TO MINIMIZE REPETITION WE SET IT AS A VARIABLE.
 */
+DWCAppModule.controller('jobIndexController', ['$scope','jobsFactory', '$cookies','$location', '$routeParams', function($scope, jobsFactory,$cookies, $location, routeParams) {
+  var logged_in_user = $cookies.get('logged_user');
+  $scope.firstName = $cookies.get("firstName");
+  $scope.userLevel = $cookies.get("userLevel");
+  $scope.lastName = $cookies.get("lastName");
+  $scope.emailAddress = $cookies.get("emailAddress");
+    
     $scope.index = function(){
         jobsFactory.getAllJobs(function(returnedData){
             $scope.jobs = returnedData;
@@ -14,6 +21,11 @@ DWCAppModule.controller('jobIndexController', ['$scope','jobsFactory', '$locatio
         $scope.index();
     };
     $scope.index();
+
+    $scope.logout = function(){
+    $cookies.remove('logged_user');
+    $location.url('/');
+  }
 /*
   OUR $scope.create function goes here <-- $scope because we need to access this method 
   with ng-submit or ng-click (from the form in the previous assignment).  
